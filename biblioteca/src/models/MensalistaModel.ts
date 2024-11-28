04609319d690b0b0956cfc599f527f388edb106c
MingRenan <100416045+MingRenan@users.noreply.github.com>
2024-11-28 18:08:34 -0300

    Update MensalistaModel.ts

--- biblioteca/src/models/MensalistaModel.ts
+++ biblioteca/src/models/MensalistaModel.ts
@@ -3,12 +3,23 @@ class MensalistaModel {
     matricula: number;
     salario:number;
     funcionario: string;
-   
+    
+//    nome:string;
+//    idade: number;
+//    fone: string;
+//    email:string;
 
-    constructor(matricula:number,salario:number, funcionario:string){
+    constructor(matricula:number,salario:number, funcionario:string 
+        // ,nome:string, idade: number, fone: string, email:string
+    ){
         this.matricula = matricula;
         this.funcionario = funcionario;
         this.salario = salario; 
+
+    //    this.nome = nome;
+    //     this.idade = idade;
+    //     this.fone = fone;
+    //     this.email = email;
         
     }
 }
